93c258947b8e58fd5256663e1dbdf4257fbc0087
Steven Riley <rileystevend@gmail.com>
2019-01-31 16:06:15 -0600

    add oci adb section to tf file

--- example.tf
+++ example.tf
@@ -18,3 +18,17 @@ output "show-ads" {
   value = "${data.oci_identity_availability_domains.ads.availability_domains}"
 }
 
+resource "oci_database_autonomous_database" "test_autonomous_database" {
+    #Required
+    admin_password = "${var.autonomous_database_admin_password}"
+    compartment_id = "${var.compartment_id}"
+    cpu_core_count = "${var.autonomous_database_cpu_core_count}"
+    data_storage_size_in_tbs = "${var.autonomous_database_data_storage_size_in_tbs}"
+    db_name = "${var.autonomous_database_db_name}"
+
+    #Optional
+    defined_tags = {"Operations.CostCenter"= "42"}
+    display_name = "${var.autonomous_database_display_name}"
+    freeform_tags = {"Department"= "Finance"}
+    license_model = "${var.autonomous_database_license_model}"
+}
